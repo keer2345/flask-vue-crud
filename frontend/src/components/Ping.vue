9e42d45fab7b9931a9e816a340df371e07522c31
keer2345 <keer2345@gmail.com>
2018-12-01 22:04:52 +0800

    axios

--- frontend/src/components/Ping.vue
+++ frontend/src/components/Ping.vue
@@ -4,12 +4,28 @@
   </div>
 </template>
 <script>
+import axios from "axios";
 export default {
   name: "Ping",
   data() {
     return {
-      msg: "Hello!"
+      msg: ""
     };
+  },
+  methods: {
+    getMessage() {
+      axios
+        .get("http://localhost:5000/ping")
+        .then(response => {
+          this.msg = response.data;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }
+  },
+  beforeMount() {
+    this.getMessage();
   }
 };
 </script>

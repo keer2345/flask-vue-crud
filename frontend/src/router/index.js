65d0410ceafe3bc0c29ed2e76f1bb984c70e4b2b
keer2345 <keer2345@gmail.com>
2018-12-01 23:19:15 +0800

    Books

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Ping from '@/components/Ping'
+import Books from '@/components/Books'
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routes: [{ path: '/', component: Ping }]
+  routes: [
+    { path: '/', name: 'Books', component: Books },
+    { path: '/ping', name: 'Ping', component: Ping }
+  ],
+  mode: 'history'
 })
 
 export default router

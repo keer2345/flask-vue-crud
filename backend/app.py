91789017f731ec81322653f51e85c803166ad8e1
keer2345 <keer2345@gmail.com>
2018-12-01 23:57:27 +0800

    Books.vue & get all_books

--- backend/app.py
+++ backend/app.py
@@ -11,10 +11,30 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app)
 
+BOOKS = [{
+    'title': 'On the Road',
+    'author': 'Jack Kerouac',
+    'read': True
+},
+         {
+             'title': 'Harry Potter and the Philosopher\'s Stone',
+             'author': 'J. K. Rowling',
+             'read': False
+         }, {
+             'title': 'Green Eggs and Ham',
+             'author': 'Dr. Seuss',
+             'read': True
+         }]
+
 
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
+
+
+@app.route('/books', methods=['GET'])
+def all_books():
+    return jsonify({'status': 'success', 'books': BOOKS})
 
 
 if __name__ == "__main__":
